b8ffba609132780a7020f43cc0b9a46a46b55773
iolq <wipawee_watt@cmu.ac.th>
2021-08-05 14:52:46 +0700

    updated

--- script.js
+++ script.js
@@ -6,8 +6,33 @@ const color = document.querySelector('#color')
 
 // define more constants and variables here
 
+
 btn_toggle.onclick = () => {
-  // your code here
+  function myFunction() {
+    var x = document.getElementById("text");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 }
+btn_search.onclick= () => {
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").toggle();
+    });
+  });
+}
+btn_reset.onclick= () => {
+  btn_reset.disabled = true
+  setTimeout(function () {
+      Text.remove()
+      saveToStorage()
+      const temp = loadFromStorage()
+  }, 1);
+}
+
+
 
 // more codes for Search and Reset buttons here
